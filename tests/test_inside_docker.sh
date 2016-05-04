3d2e7833506d401b25635294deb021ff35643ece
Derek Weitzel <djw8605@gmail.com>
2016-05-04 16:25:01 -0500

    Adding useradd and group add for gracc

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -18,6 +18,9 @@ pip install -r requirements.txt
 # Install and Start overmind
 python setup.py install
 which graccreq
+groupadd -r gracc
+useradd -r -s /bin/false -G gracc gracc
+
 mkdir -p /etc/gracc/config.d/
 cp tests/gracc-request-test.toml /etc/gracc/config.d/gracc-request.toml
 cp config/graccreq.service /lib/systemd/system/
