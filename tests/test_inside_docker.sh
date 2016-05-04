304c2266fba6fbfb9040b470370ec34799419d12
Derek Weitzel <djw8605@gmail.com>
2016-05-04 13:11:11 -0500

    Adding tests

---
+++ tests/test_inside_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -xe
+
+# Install all the things
+pip install -r /gracc-request/requirements.txt
+
+# Install and Start overmind
+python setup.py install
+graccreq -c /gracc-request/tests/gracc-request-test.toml &
+overmind_pid=$!
+
+python /gracc-request/tests/test.py
+
+kill $overmind_pid
+
+
---
+++ tests/test_overmind.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+import gracc_reporter
+
+overmind = gracc_reporter.OverMind.OverMind('gracc-reporter-test.toml')
+
+overmind.run()
+
