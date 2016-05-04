2d653402f649e1ac2ad96574e8a741fb4d5917a1
Derek Weitzel <djw8605@gmail.com>
2016-05-04 14:33:14 -0500

    Add install and start elasticsearch and rabbitmq

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -5,6 +5,13 @@
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y update
 yum -y install python-pip
+
+yum -y install rabbitmq-server java-1.8.0-openjdk
+rpm -Uvh https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.3.2/elasticsearch-2.3.2.rpm
+
+sudo systemctl start elasticsearch.service
+sudo systemctl start rabbitmq-server.service
+
 cd /gracc-request
 pip install -r requirements.txt
 
