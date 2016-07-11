e92125619878034421191183c21aa1b27909315d
Derek Weitzel <djw8605@gmail.com>
2016-07-11 15:25:56 -0500

    Adding RPM (#10)
    Add RPM configuration and test it!

---
+++ config/gracc-request.spec
@@ -0,0 +1,75 @@
+Name:           gracc-request
+Version:        1.0
+Release:        1%{?dist}
+Summary:        GRACC Listener for Raw and Summary Records
+
+License:        ASL 2.0
+URL:            https://opensciencegrid.github.io/gracc/
+Source0:        gracc-request-%{version}.tar.gz
+BuildArch:      noarch
+
+BuildRequires:  python-setuptools
+BuildRequires:  systemd
+BuildRequires:  python-srpm-macros 
+BuildRequires:  python-rpm-macros 
+BuildRequires:  python2-rpm-macros 
+BuildRequires:  epel-rpm-macros
+BuildRequires:  systemd
+Requires:       python2-pika
+Requires:       python-elasticsearch-dsl
+Requires:       python-dateutil
+Requires:       python-toml
+Requires(pre):  shadow-utils
+
+%description
+GRACC Listener for Raw and Summary Records
+
+%package -n %{name}-client
+Summary:        GRACC Listener Client
+Requires:       python2-pika
+Requires:       python-dateutil
+%description -n %{name}-client
+GRACC Listener for Raw and Summary Records
+
+
+%pre
+getent group gracc >/dev/null || groupadd -r gracc
+getent passwd gracc >/dev/null || \
+    useradd -r -g gracc -d /tmp -s /sbin/nologin \
+    -c "GRACC Services Account" gracc
+exit 0
+
+%prep
+%setup -q
+
+
+%build
+%{py2_build}
+
+
+%install
+%{py2_install}
+
+
+install -d -m 0755 $RPM_BUILD_ROOT/%{_sysconfdir}/graccreq/config.d/
+install -m 0744 config/gracc-request.toml $RPM_BUILD_ROOT/%{_sysconfdir}/graccreq/config.d/gracc-request.toml
+install -d -m 0755 $RPM_BUILD_ROOT/%{_unitdir}
+install -m 0744 config/graccreq.service $RPM_BUILD_ROOT/%{_unitdir}/
+
+
+
+%files
+%defattr(-, gracc, gracc)
+%{python2_sitelib}/graccreq
+%{python2_sitelib}/graccreq-%{version}-py2.?.egg-info
+%attr(755, root, root) %{_bindir}/*
+%{_unitdir}/graccreq.service
+%config %{_sysconfdir}/graccreq/config.d/gracc-request.toml
+
+%doc
+
+
+
+%changelog
+
+
--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -3,30 +3,43 @@
 
 # Install all the things
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+curl -o /etc/yum.repos.d/djw8605-GRACC-epel-7.repo https://copr.fedorainfracloud.org/coprs/djw8605/GRACC/repo/epel-7/djw8605-GRACC-epel-7.repo 
 yum -y update
 
-yum -y install python-pip git rabbitmq-server java-1.8.0-openjdk
+yum -y install python-pip git rabbitmq-server java-1.8.0-openjdk python-elasticsearch-dsl rpm-build python-srpm-macros python-rpm-macros python2-rpm-macros epel-rpm-macros
 rpm -Uvh https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.3.2/elasticsearch-2.3.2.rpm
 
 systemctl start elasticsearch.service
 systemctl start rabbitmq-server.service
 
-cd /gracc-request
-pip install -r requirements.txt
+# Prepare the RPM environment
+mkdir -p /tmp/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+cat >> /etc/rpm/macros.dist << EOF
+%dist .osg.el${OS_VERSION}
+%osg 1
+EOF
 
-# Install and Start overmind
-python setup.py install
-groupadd -r gracc
-useradd -r -s /bin/false -g gracc gracc
+cp gracc-request/config/gracc-request.spec /tmp/rpmbuild/SPECS
+package_version=`grep Version gracc-request/config/gracc-request.spec | awk '{print $2}'`
+pushd gracc-request
+git archive --format=tar --prefix=gracc-request-${package_version}/ HEAD  | gzip >/tmp/rpmbuild/SOURCES/gracc-request-${package_version}.tar.gz
+popd
 
-mkdir -p /etc/gracc/config.d/
-cp tests/gracc-request-test.toml /etc/gracc/config.d/gracc-request.toml
-cp config/graccreq.service /lib/systemd/system/
+# Build the RPM
+rpmbuild --define '_topdir /tmp/rpmbuild' -ba /tmp/rpmbuild/SPECS/gracc-request.spec
+
+yum localinstall -y /tmp/rpmbuild/RPMS/noarch/gracc-request*
+
+# Copy in the test configuration
+cp -f gracc-request/tests/gracc-request-test.toml /etc/graccreq/config.d/gracc-request.toml
+
 systemctl start graccreq.service
 
 # Wait for the overmind to start up
 sleep 10
-#journalctl -u graccreq.service --no-pager
+journalctl -u graccreq.service --no-pager
+
+ls -l /usr/bin/graccreq
 
 # Install the test data
 curl -O https://nodejs.org/dist/v4.4.4/node-v4.4.4-linux-x64.tar.xz
@@ -39,9 +52,11 @@ pushd gracc-test-data
 bash -x ./import.sh
 popd
 
+pushd gracc-request
 python -m unittest discover tests/unittests "test_*.py"
+popd
 
-sleep 1
+sleep 30
 journalctl -u graccreq.service --no-pager -n 20
 
 
