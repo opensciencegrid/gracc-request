6e9a864fdf8c285ec6a34c14e16244649598a4f7
Derek Weitzel <djw8605@gmail.com>
2016-08-01 16:43:40 -0500

    Change to EndTime based replays

--- src/graccreq/raw_replayer.py
+++ src/graccreq/raw_replayer.py
@@ -54,7 +54,7 @@ class RawReplayer(replayer.Replayer):
         
         logging.debug("Beginning search")
         s = Search(using=client, index='gracc.osg.raw0-*')
-        s = s.filter('range', **{'@timestamp': {'from': from_date, 'to': to_date }})
+        s = s.filter('range', **{'EndTime': {'from': from_date, 'to': to_date }})
         
         logging.debug("About to execute query:\n%s" % str(s.to_dict()))
         
