fdbe0f62d22ca28f93b1ea0ea2ab674203a5a815
sbhat <sbhat@fnal.gov>
2016-09-26 10:06:25 -0500

    Moved organized unittests back into repo

--- tests/unittests/test_OIMTopology.py
+++ tests/unittests/test_OIMTopology.py
@@ -6,10 +6,32 @@ from graccreq.oim import OIMTopology
 
 
 class BasicOIMTopologyTests(unittest.TestCase):
-    def setUp(self):
+    def setUpClass(cls):
         self.topology = OIMTopology.OIMTopology()
 
-    def test_all(self):
+    def test_fqdn(self):
+        """OIMTopology match by gracc probe to topology FQDN"""
+        testdict = self.topology.get_information_by_fqdn('fifebatch1.fnal.gov')
+        self.assertEqual(testdict['Facility'], 'Fermi National Accelerator'
+                                               ' Laboratory')
+        self.assertEqual(testdict['Site'], 'FermiGrid')
+        self.assertEqual(testdict['ResourceGroup'], 'FNAL_FIFE_SUBMIT')
+        self.assertEqual(testdict['Resource'],'FIFE_SUBMIT_1')
+        return True
+
+    def test_resource(self):
+        """OIMTopology match by gracc SiteName to topology resource"""
+        testdict = self.topology.get_information_by_resource('AGLT2_SL6')
+        self.assertEqual(testdict['Facility'], 'University of Michigan')
+        self.assertEqual(testdict['Site'], 'AGLT2')
+        self.assertEqual(testdict['ResourceGroup'], 'AGLT2')
+        self.assertEqual(testdict['Resource'],'AGLT2_SL6')
+        return True
+
+
+class GRACCDictTests(BasicOIMTopologyTests):
+    def setUp(self):
+        super(GRACCDictTests, self).setUp()
         self.testdoc_op = {'SiteName': 'AGLT2_SL6', 'VOName': 'Fermilab',
                            'ProbeName': 'condor:gate02.grid.umich.edu'}
         self.testdoc_ded = {'SiteName': 'AGLT2_SL6', 'VOName': 'ATLAS',
@@ -28,40 +50,41 @@ class BasicOIMTopologyTests(unittest.TestCase):
                              'VOName': 'Fermilab',
                              'ProbeName':
                                  'condor:gate02.grid.umich.edu1231231'}
+        self.topology2 = OIMTopology.OIMTopology()
 
-        # FQDN lookup
-        testdict = self.topology.get_information_by_fqdn('fifebatch1.fnal.gov')
-        self.assertEqual(testdict['Facility'], 'Fermi National Accelerator'
-                                               ' Laboratory')
-        self.assertEqual(testdict['Site'], 'FermiGrid')
-        self.assertEqual(testdict['ResourceGroup'], 'FNAL_FIFE_SUBMIT')
-        self.assertEqual(testdict['Resource'],'FIFE_SUBMIT_1')
+    def test_blankdict(self):
+        """If URL retrieval fails or parsing didn't work, we should get a
+        blank dictionary"""
+        self.topology2.xml_file = None  # URL retrieval or parsing didn't work
+        test_dict = self.topology2.generate_dict_for_gracc(self.testdoc_ded)
+        self.assertFalse(test_dict)
+        return True
 
-        # Resource lookup
-        testdict = self.topology.get_information_by_resource('AGLT2_SL6')
-        self.assertEqual(testdict['Facility'], 'University of Michigan')
-        self.assertEqual(testdict['Site'], 'AGLT2')
-        self.assertEqual(testdict['ResourceGroup'], 'AGLT2')
-        self.assertEqual(testdict['Resource'],'AGLT2_SL6')
-
-
-        # Dedicated -- incorrect SiteName should be ignored
+    def test_dedicated(self):
+        """Matching by probename, so the incorrect SiteName should be
+        ignored"""
         ded = self.topology.generate_dict_for_gracc(self.testdoc_ded)
         self.assertEqual(ded['Facility'], 'University of Michigan')
         self.assertEqual(ded['Site'], 'AGLT2')
         self.assertEqual(ded['ResourceGroup'], 'AGLT2')
         self.assertEqual(ded['Resource'],'AGLT2_CE_2')
         self.assertEqual(ded['UsageModel'], 'DEDICATED')
+        return True
 
-        # Opportunistic -- incorrect SiteName should be ignored
+    def test_opportunistic(self):
+        """Matching by probename, so the incorrect SiteName should be
+        ignored"""
         op = self.topology.generate_dict_for_gracc(self.testdoc_op)
         self.assertEqual(op['Facility'], 'University of Michigan')
         self.assertEqual(op['Site'], 'AGLT2')
         self.assertEqual(op['ResourceGroup'], 'AGLT2')
         self.assertEqual(op['Resource'], 'AGLT2_CE_2')
         self.assertEqual(op['UsageModel'], 'OPPORTUNISTIC')
+        return True
 
-        # Probe name is wrong, so should match on site name to Resource Group
+    def test_fallbacktosite(self):
+        """Probe name is wrong, so should match on site name to Resource 
+        Group"""
         fail_probe = self.topology.generate_dict_for_gracc(
             self.testdoc_fail_probe)
         self.assertEqual(fail_probe['Facility'], 'University of Michigan')
@@ -70,38 +93,37 @@ class BasicOIMTopologyTests(unittest.TestCase):
         self.assertNotEqual(fail_probe['Resource'],'AGLT2_CE_2')
         self.assertEqual(fail_probe['Resource'],'AGLT2_SL6')
         self.assertEqual(fail_probe['UsageModel'], 'OPPORTUNISTIC')
-
-
-        # Probe name and SiteName are wrong
+        return True
+    
+    def test_fail(self):
+        """Probe name and SiteName are wrong"""
         fail_dict = self.topology.generate_dict_for_gracc(
             self.testdoc_fail)
         self.assertEqual(fail_dict, {})
+        return True
 
-        # No Probe name in gracc doc at all"""
+    def test_noprobe(self):
+        """No Probe name in gracc doc at all"""
         fail_probe = self.topology.generate_dict_for_gracc(
             self.testdoc_noprobe)
         self.assertEqual(fail_probe['Facility'], 'University of Michigan')
         self.assertEqual(fail_probe['Site'], 'AGLT2')
         self.assertEqual(fail_probe['ResourceGroup'], 'AGLT2')
-        self.assertNotEqual(fail_probe['Resource'], 'AGLT2_CE_2')
-        self.assertEqual(fail_probe['Resource'], 'AGLT2_SL6')
+        self.assertNotEqual(fail_probe['Resource'],'AGLT2_CE_2')
+        self.assertEqual(fail_probe['Resource'],'AGLT2_SL6')
         self.assertEqual(fail_probe['UsageModel'], 'OPPORTUNISTIC')
+        return True
 
-        # No probe name or site name in gracc record
+    def test_noprobe_nosite(self):
         fail_probe = self.topology.generate_dict_for_gracc(
             self.testdoc_nositeorprobe)
         self.assertFalse(fail_probe)
+        return True
 
-        # No VO name in gracc record
+    def test_no_vo(self):
         fail_probe = self.topology.generate_dict_for_gracc(
-        self.testdoc_no_vo)
+            self.testdoc_no_vo)
         self.assertEqual(fail_probe['UsageModel'], 'UNKNOWN')
-
-        # Blank Dictionary
-        self.topology.xml_file = None  # URL retrieval or parsing didn't work
-        test_dict = self.topology.generate_dict_for_gracc(self.testdoc_ded)
-        self.assertFalse(test_dict)
-
         return True
 
 
