94ca01d6cd8809d4c8b4326cad5032589360599d
Derek Weitzel <djw8605@gmail.com>
2021-01-29 13:50:58 -0600

    Use the URI specified in the configuration for the ES connection

--- src/graccreq/transfer_summary.py
+++ src/graccreq/transfer_summary.py
@@ -54,7 +54,8 @@ class TransferSummary(summary_replayer.SummaryReplayer):
 
     def _queryElasticsearch(self, from_date, to_date, query):
         logging.debug("Connecting to ES")
-        client = Elasticsearch(timeout=300)
+        es_uri = self._config['ElasticSearch'].get('uri', 'http://localhost:9200')
+        client = Elasticsearch(es_uri, timeout=300)
         
         # For summaries, we only summarize full days, so strip the time from the from & to
         # Round the date up, so we get the entire last day they requested.
