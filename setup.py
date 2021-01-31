55df20ce6b8ceebaec8d7d0061c8d03706edeb0c
Derek Weitzel <djw8605@gmail.com>
2021-01-30 18:17:14 -0600

    Bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 
 setup(name='graccreq',
-      version='3.12.1',
+      version='3.12.3',
       description='GRACC Request Daemon',
       author_email='dweitzel@cse.unl.edu',
       author='Derek Weitzel',
