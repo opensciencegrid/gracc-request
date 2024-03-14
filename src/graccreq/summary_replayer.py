2d9e07480c61f348bb4abc3ff10c4d8b7d82b1bd
Derek Weitzel <djw8605@gmail.com>
2024-03-14 11:15:11 -0500

    Fixing elasticsearch call

--- src/graccreq/summary_replayer.py
+++ src/graccreq/summary_replayer.py
@@ -159,7 +159,7 @@ class SummaryReplayer(replayer.Replayer):
     def _queryElasticsearch(self, from_date, to_date, query):
         logging.debug("Connecting to ES")
         es_uri = self._config['ElasticSearch'].get('uri', 'http://localhost:9200')
-        client = Elasticsearch(es_uri, timeout=300)
+        client = OpenSearch(es_uri, timeout=300)
         
         # For summaries, we only summarize full days, so strip the time from the from & to
         # Round the date up, so we get the entire last day they requested.
